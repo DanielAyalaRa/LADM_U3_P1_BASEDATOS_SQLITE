d1a000ac8dbfe1b4e90f83c84274ea239addf962
DanielAyalaRa <daalayalar@ittepic.edu.mx>
2021-11-10 22:38:48 -0700

    Primer commit LADM_U3_P1 1.0

---
+++ LADM_U3_P1/app/src/main/java/mx/tecm/tepic/ladm_u3_p1_bd/MainActivity.kt
@@ -0,0 +1,11 @@
+package mx.tecm.tepic.ladm_u3_p1_bd
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
